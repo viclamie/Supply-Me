0675f68ca99138eb4794314260a377d3d39cb818
viclamie <85850525+viclamie@users.noreply.github.com>
2021-06-17 23:49:48 -0400

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.supply;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView tasksRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        tasksRecyclerView = findViewById(R.id.tasksRecyclerView);
+        tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
